53c6c11f70f2e5867e84b15af2b490ffe3296354
Mona <xiaomei.mona.he@gmail.com>
2025-12-14 22:49:57 -0700

    fix: fix login pages logic errors

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import LoginClient from "./LoginClient";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense fallback={null}>
